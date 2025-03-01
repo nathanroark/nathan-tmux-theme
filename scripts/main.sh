40d1a4fa8215310b52f38a09b8ffc6226ada74d0
Nathan Roark <nathanroark@protonmail.com>
2025-03-01 13:09:21 -0600

    swap

--- scripts/main.sh
+++ scripts/main.sh
@@ -104,8 +104,8 @@ main()
   # Horizon Bright Color Palette (light mode)
   # https://github.com/Gogh-Co/Gogh/blob/master/themes/Horizon%20Bright.yml
   white='#1c1e26 '        # Foreground Text
-  gray='#FADAD1'                # Highlight Background
-  dark_gray='#FDF0ED'     # Background
+  dark_gray='#FADAD1'                # Highlight Background
+  gray='#FDF0ED'     # Background
   # green='#1EB980'         # Green
   green='#07DA8C'         # Light Green
   light_purple='#EE64AE'  # Red
