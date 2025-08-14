c1ac113dc4922b66d154de03b9ff9b1291bce201
Nathan Roark <nathanroark@protonmail.com>
2025-08-14 09:08:23 -0500

    update

--- scripts/main.sh
+++ scripts/main.sh
@@ -68,22 +68,6 @@ main()
 
 
   # # Rosé Pine Color Palette
-  # white='#e0def4'        # Text
-  # gray='#121214'         # Highlight Background
-  # dark_gray='#121214'    # Background
-  # # gray='#6e6a86'       # Muted
-  # # dark_gray='#191724'  # Base
-  # light_purple='#c4a7e7' # Iris
-  # dark_purple='#2a283e'  # Highlight High
-  # blue='#3e8fb0'         # Pine (moon)
-  # cyan='#9ccfd8'         # Foam
-  # green='#31748f'        # Pine
-  # orange='#f6c177'       # Gold
-  # red='#eb6f92'          # Love
-  # pink='#ebbcba'         # Rose
-  # yellow='#f6d177'       # Subtle
-
-  # Rosé Pine Color Palette
   white='#e0def4'        # Text
   gray='#121214'         # Highlight Background
   dark_gray='#121214'    # Background
@@ -93,11 +77,27 @@ main()
   dark_purple='#2a283e'  # Highlight High
   blue='#3e8fb0'         # Pine (moon)
   cyan='#9ccfd8'         # Foam
-  green='#98ff98' # old green before darker
+  green='#31748f'        # Pine
   orange='#f6c177'       # Gold
   red='#eb6f92'          # Love
   pink='#ebbcba'         # Rose
   yellow='#f6d177'       # Subtle
+
+  # Rosé Pine Color Palette
+  # white='#e0def4'        # Text
+  # gray='#121214'         # Highlight Background
+  # dark_gray='#121214'    # Background
+  # # gray='#6e6a86'       # Muted
+  # # dark_gray='#191724'  # Base
+  # light_purple='#c4a7e7' # Iris
+  # dark_purple='#2a283e'  # Highlight High
+  # blue='#3e8fb0'         # Pine (moon)
+  # cyan='#9ccfd8'         # Foam
+  # green='#98ff98' # old green before darker
+  # orange='#f6c177'       # Gold
+  # red='#eb6f92'          # Love
+  # pink='#ebbcba'         # Rose
+  # yellow='#f6d177'       # Subtle
 
 
 
